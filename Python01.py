4edc968bb20963f5efe9f0953d448eb55257f4d0
woshilaoxu <923343691@qq.com>
2018-05-07 19:37:26 +0800

    添加来了一个九九乘法表

--- Python01.py
+++ Python01.py
@@ -4,3 +4,7 @@ print("Python是一门跨平台编辑语言")
 print("阿甫,这笨蛋还没弄好！")
 print("来呀同志们，晚上一起玩王者荣耀！")
 print("李振，我把first文件删了")
+for i in range(1,10):
+    for j in range(1,i+1):
+        print("%dX%d=%d"%(j,i,i*j),end=" ")
+    print(" ")
